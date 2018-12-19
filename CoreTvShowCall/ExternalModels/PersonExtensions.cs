a9af8eee2edf8ec54ac69ebcd26174af6822d5ba
Daan Acohen <daan.acohen@connectingapps.net>
2018-12-19 14:44:47 +0100

    Parse birthday

---
+++ CoreTvShowCall/ExternalModels/PersonExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CoreTvShowCall.ExternalModels
+{
+    public static class PersonExtensions
+    {
+        public static DateTime? GetBirthDate(this Person person)
+        {
+            var successFullyParsed = DateTime.TryParse(person.birthday, out var result);
+            if (successFullyParsed)
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
