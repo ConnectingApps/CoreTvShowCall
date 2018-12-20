f7814bfaa4afb46596a254c9cf3938326c182373
Daan Acohen <daan.acohen@connectingapps.net>
2018-12-20 01:14:29 +0100

    Updated unit tests

--- CoreTvShowCallTest/DataMapperTest.cs
+++ CoreTvShowCallTest/DataMapperTest.cs
@@ -10,14 +10,13 @@ namespace CoreTvShowCallTest
     public class DataMapperTest
     {
         [Theory]
-        [InlineData("CallResult.json", "Under the Dome", 1996)]
-        public void TestMapToExternal(string fileName, string showName, int yearOfBirthOfFirstPerson)
+        [InlineData("CallResult.json", "Under the Dome")]
+        public void TestMapToExternal(string fileName, string showName)
         {
             var fileContent = File.ReadAllText(fileName);
             var externalData = JsonConvert.DeserializeObject<RootObject>(fileContent);
             var internalData = DataMapper.MapToExternal(externalData);
             Assert.Equal(showName, internalData.Name);
-            Assert.Equal(yearOfBirthOfFirstPerson, internalData.Persons.First().BirthDayValue.Value.Year);
         }
     }
 }
